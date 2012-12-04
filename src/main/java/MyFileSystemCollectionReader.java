f305b48d0f924e103bf85c32559d9f57867b5209
sykim82 <suyoun@cs.cmu.edu>
2012-12-04 01:11:35 -0500

    fix the path problem in collection reader

--- src/main/java/MyFileSystemCollectionReader.java
+++ src/main/java/MyFileSystemCollectionReader.java
@@ -21,162 +21,177 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import org.apache.uima.resource.ResourceConfigurationException;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.util.Level;
+import org.apache.uima.util.FileUtils;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
 
 /**
- * A collection reader that reads documents from a directory in the
- * filesystem.
+ * A simple collection reader that reads documents from a directory in the filesystem. It can be
+ * configured with the following parameters:
+ * <ul>
+ * <li><code>InputDirectory</code> - path to directory containing files</li>
+ * <li><code>Encoding</code> (optional) - character encoding of the input files</li>
+ * <li><code>Language</code> (optional) - language of the input documents</li>
+ * </ul>
  * 
- * This resource is different from the one in UIMA example as it uses TIKA to
- * extract the text from binary documents and generates annotations to represent
- * the markup
- */
-
-
-/**
- * Annotator class that annotates Tokens and Sentences. and annotates Gene names.
- * @author suyoun kim
- * @param 
- * @return 
+ * 
  */
 public class MyFileSystemCollectionReader extends CollectionReader_ImplBase {
-	/**
-	 * Name of configuration parameter that must be set to the path of a
-	 * directory containing input files.
-	 */
-	public static final String PARAM_INPUTDIR = "InputDirectory";
+  /**
+   * Name of configuration parameter that must be set to the path of a directory containing input
+   * files.
+   */
+  public static final String PARAM_INPUTDIR = "InputFile";
 
-	/**
-	 * Name of optional configuration parameter that contains the language of
-	 * the documents in the input directory. If specified this information will
-	 * be added to the CAS.
-	 */
-	public static final String PARAM_LANGUAGE = "Language";
+  /**
+   * Name of configuration parameter that contains the character encoding used by the input files.
+   * If not specified, the default system encoding will be used.
+   */
+  public static final String PARAM_ENCODING = "Encoding";
 
-	private final static String PARAM_MIME = "MIME";
-	
-	private final static String tika_file_param = "tikaConfigFile";
+  /**
+   * Name of optional configuration parameter that contains the language of the documents in the
+   * input directory. If specified this information will be added to the CAS.
+   */
+  public static final String PARAM_LANGUAGE = "Language";
 
-	private String mLanguage;
-	
-	private String mMIME;
-	
-	private List<File> mFiles;
-	
-	private int mCurrentIndex;
+  /**
+   * Name of optional configuration parameter that indicates including
+   * the subdirectories (recursively) of the current input directory.
+   */
+  public static final String PARAM_SUBDIR = "BrowseSubdirectories";
+  
+  private ArrayList<File> mFiles;
 
+  private String mEncoding;
 
+  private String mLanguage;
+  
+  private Boolean mRecursive;
 
-	/**
-	 * @see org.apache.uima.collection.CollectionReader#hasNext()
-	 */
-	public boolean hasNext() {
-		return mCurrentIndex < mFiles.size();
-	}
+  private int mCurrentIndex;
 
-	/**
-	 * @see org.apache.uima.collection.CollectionReader#getNext(org.apache.uima.cas.CAS)
-	 */
-	public void getNext(CAS aCAS) throws IOException, CollectionException {
-		JCas jcas;
-		try {
-			jcas = aCAS.getJCas();
-		} catch (CASException e) {
-			throw new CollectionException(e);
-		}
+  /**
+   * @see org.apache.uima.collection.CollectionReader_ImplBase#initialize()
+   */
+  public void initialize() throws ResourceInitializationException {
+    File fname = new File(((String) getConfigParameterValue(PARAM_INPUTDIR)).trim());
+    mEncoding  = (String) getConfigParameterValue(PARAM_ENCODING);
+    mLanguage  = (String) getConfigParameterValue(PARAM_LANGUAGE);
+    mRecursive = (Boolean) getConfigParameterValue(PARAM_SUBDIR);
+    if (null == mRecursive) { // could be null if not set, it is optional
+      mRecursive = Boolean.FALSE;
+    }
+    mCurrentIndex = 0;
 
-		// open input stream to file
-		File file = (File) mFiles.get(mCurrentIndex++);
-/*
-		// call Tika wrapper 
-		try {
-			tika.populateCASfromURL(aCAS, file.toURI().toURL(), this.mMIME, this.mLanguage);
-		} catch (CASException e) {
-		  String msg = String.format("Problem converting file: %s\t%s%n", file.toURI().toURL(), e.getMessage());
-			getLogger().log(Level.WARNING, msg);
-			throw new IOException(msg);
-	    	//jcas.setDocumentText(" "); return;
-		}
-		
-*/
-	}
+    /*
+    // if input directory does not exist or is not a directory, throw exception
+    if (!directory.exists() || !directory.isDirectory()) {
+      throw new ResourceInitializationException(ResourceConfigurationException.DIRECTORY_NOT_FOUND,
+              new Object[] { PARAM_INPUTDIR, this.getMetaData().getName(), directory.getPath() });
+    }
+    */
 
-	/**
-	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#close()
-	 */
-	public void close() throws IOException {
-	}
+    // get list of files in the specified directory, and subdirectories if the
+    // parameter PARAM_SUBDIR is set to True
+    mFiles = new ArrayList<File>();
+    addFilesFromDir(fname);
+  }
+  
+  /**
+   * This method adds files in the directory passed in as a parameter to mFiles.
+   * If mRecursive is true, it will include all files in all
+   * subdirectories (recursively), as well. 
+   * 
+   * @param dir
+   */
+  private void addFilesFromDir(File fname) {
+    mFiles.add(fname);
+    /*
+    File[] files = dir.listFiles();
+    for (int i = 0; i < files.length; i++) {
+      if (!files[i].isDirectory()) {
+        mFiles.add(files[i]);
+      } else if (mRecursive) {
+        addFilesFromDir(files[i]);
+      }
+    }
+    */
+  }
 
-	/**
-	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#getProgress()
-	 */
-	public Progress[] getProgress() {
-		return new Progress[] { new ProgressImpl(mCurrentIndex, mFiles.size(),
-				Progress.ENTITIES) };
-	}
+  /**
+   * @see org.apache.uima.collection.CollectionReader#hasNext()
+   */
+  public boolean hasNext() {
+    return mCurrentIndex < mFiles.size();
+  }
 
-	/**
-	 * Gets the total number of documents that will be returned by this
-	 * collection reader. This is not part of the general collection reader
-	 * interface.
-	 * 
-	 * @return the number of documents in the collection
-	 */
-	public int getNumberOfDocuments() {
-		return mFiles.size();
-	}
+  /**
+   * @see org.apache.uima.collection.CollectionReader#getNext(org.apache.uima.cas.CAS)
+   */
+  public void getNext(CAS aCAS) throws IOException, CollectionException {
+    JCas jcas;
+    try {
+      jcas = aCAS.getJCas();
+    } catch (CASException e) {
+      throw new CollectionException(e);
+    }
 
-	@Override
-	public void initialize() throws ResourceInitializationException {
-		File directory = new File(
-				((String) getConfigParameterValue(PARAM_INPUTDIR)).trim());
-		mLanguage = (String) getConfigParameterValue(PARAM_LANGUAGE);
-		mCurrentIndex = 0;
+    // open input stream to file
+    File file = (File) mFiles.get(mCurrentIndex++);
+    String text = FileUtils.file2String(file, mEncoding);
+      // put document in CAS
+    jcas.setDocumentText(text);
 
-		mMIME = (String) getConfigParameterValue(MyFileSystemCollectionReader.PARAM_MIME);
-		
-		// initialise TIKA parser
-		/*
-		String tikaConfigURL = (String) getConfigParameterValue(tika_file_param);
-		try {
-			tika = new TIKAWrapper(tikaConfigURL);
-		} catch (TikaException e) {
-			throw new ResourceInitializationException(e);
-		}
-		*/
-		
-		// if input directory does not exist or is not a directory, throw
-		// exception
-		
-		if (!directory.exists() || !directory.isDirectory()) {
-			throw new ResourceInitializationException(
-					ResourceConfigurationException.DIRECTORY_NOT_FOUND,
-					new Object[] { PARAM_INPUTDIR,
-							this.getMetaData().getName(), directory.getPath() });
-		}
-		
+    // set language if it was explicitly specified as a configuration parameter
+    if (mLanguage != null) {
+      ((DocumentAnnotation) jcas.getDocumentAnnotationFs()).setLanguage(mLanguage);
+    }
 
-		// get list of files (not subdirectories) in the specified directory
-		mFiles = new ArrayList<File>();
-		
-		File[] files = directory.listFiles();
-		for (int i = 0; i < files.length; i++) {
-			if (!files[i].isDirectory()) {
-				mFiles.add(files[i]);
-			}
-		}
-		
-	}
+    // Also store location of source document in CAS. This information is critical
+    // if CAS Consumers will need to know where the original document contents are located.
+    // For example, the Semantic Search CAS Indexer writes this information into the
+    // search index that it creates, which allows applications that use the search index to
+    // locate the documents that satisfy their semantic queries.
+    SourceDocumentInformation srcDocInfo = new SourceDocumentInformation(jcas);
+    srcDocInfo.setUri(file.getAbsoluteFile().toURL().toString());
+    srcDocInfo.setOffsetInSource(0);
+    srcDocInfo.setDocumentSize((int) file.length());
+    srcDocInfo.setLastSegment(mCurrentIndex == mFiles.size());
+    srcDocInfo.addToIndexes();
+  }
+
+  /**
+   * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#close()
+   */
+  public void close() throws IOException {
+  }
+
+  /**
+   * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#getProgress()
+   */
+  public Progress[] getProgress() {
+    return new Progress[] { new ProgressImpl(mCurrentIndex, mFiles.size(), Progress.ENTITIES) };
+  }
+
+  /**
+   * Gets the total number of documents that will be returned by this collection reader. This is not
+   * part of the general collection reader interface.
+   * 
+   * @return the number of documents in the collection
+   */
+  public int getNumberOfDocuments() {
+    return mFiles.size();
+  }
 
 }
